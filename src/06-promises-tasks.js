5cdaa54df040cc3c1b5eb5ac9377368146c094de
Kate Mihalovskaya <kate.mihalovskaya@gmail.com>
2023-07-02 04:16:45 +0200

    feat: implement promises tasks

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,16 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    if (typeof isPositiveAnswer !== 'boolean') {
+      reject(new Error('Wrong parameter is passed! Ask her again.'));
+    } else if (isPositiveAnswer) {
+      resolve('Hooray!!! She said "Yes"!');
+    } else {
+      resolve('Oh no, she said "No".');
+    }
+  });
 }
 
 
@@ -48,8 +56,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -71,8 +79,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
@@ -92,9 +100,32 @@ function getFastestPromise(/* array */) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+function chainPromises(array, action) {
+  return new Promise((resolve, reject) => {
+    let result;
+
+    const processNextPromise = (index) => {
+      if (index >= array.length) {
+        resolve(result);
+        return;
+      }
+
+      const currentPromise = array[index];
+      currentPromise
+        .then((value) => {
+          result = result === undefined ? value : action(result, value);
+          processNextPromise(index + 1);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    };
+
+    processNextPromise(0);
+  })
+    .catch(() => {}); // Add a catch handler to suppress any unhandled rejections
 }
+
 
 module.exports = {
   willYouMarryMe,
